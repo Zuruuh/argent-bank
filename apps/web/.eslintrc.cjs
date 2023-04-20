7acd3ba2bdbadd788d1079b1459a438a8e0c763b
Zuruuh <ziadi.mail.pro@gmail.com>
2023-04-20 18:02:41 +0200

    🔧 Setup eslint & prettier

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -1,14 +1,30 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-refresh', 'prettier'],
   rules: {
+    'prettier/prettier': ['error'],
+    'react/react-in-jsx-scope': ['off'],
     'react-refresh/only-export-components': 'warn',
   },
-}
+  settings: {
+    react: {
+      version: '18.2',
+    },
+  },
+};
