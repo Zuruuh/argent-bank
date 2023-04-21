db919d5c531fa47512fb3135b1c7f9fed4a4ee79
Zuruuh <ziadi.mail.pro@gmail.com>
2023-04-21 03:39:00 +0200

    ✅ Rework navbar tests

--- apps/web/src/shared/components/Layout/components/Navigation/Navigation.spec.tsx
+++ apps/web/src/shared/components/Layout/components/Navigation/Navigation.spec.tsx
@@ -1,10 +1,29 @@
 import { render } from '@testing-library/react';
-import { test } from 'vitest';
-import Navigation from './Navigation';
+import { expect, test } from 'vitest';
 import { MemoryRouter } from 'react-router';
+import { SignInPageConfig } from '~/pages/SignInPage';
+import Navigation from './Navigation';
 
 test('It renders', async () => {
   render(<Navigation />, {
     wrapper: MemoryRouter,
   });
+});
+
+test("The navlink is active on it's url", async () => {
+  const { container } = render(
+    <MemoryRouter initialEntries={[SignInPageConfig.path]}>
+      <Navigation />
+    </MemoryRouter>
+  );
+
+  const link = container.querySelector(`a[href="${SignInPageConfig.path}"]`);
+  expect(link).to.have.class('activeLink');
+});
+
+test('The navlink is not active on another url', async () => {
+  const { container } = render(<Navigation />, { wrapper: MemoryRouter });
+
+  const link = container.querySelector(`a[href="${SignInPageConfig.path}"]`);
+  expect(link).not.to.have.class('activeLink');
 });
